50aa6939fb7f619725ef06422ca5a9d7813d3690
Harald Hoyer <harald@redhat.com>
2017-05-31 16:21:34 +0200

    removed vivoactive_hr
    device does not support GPX or FIT

--- properties.mk
+++ properties.mk
@@ -1,5 +1,5 @@
 DEVICE = fenix5
 SDK_HOME = $(HOME)/connect-iq
 DEPLOY = /run/media/$(USER)/GARMIN/GARMIN/APPS
-SUPPORTED_DEVICES_LIST = fenix5 fenix5s fenix5x fenixchronos fr735xt fr935 edge_1000 edge_520 vivoactive_hr edge820 oregon7xx rino7xx
+SUPPORTED_DEVICES_LIST = fenix5 fenix5s fenix5x fenixchronos fr735xt fr935 edge_1000 edge_520 edge820 oregon7xx rino7xx
 PRIVATE_KEY = $(HOME)/.id_rsa_garmin.der
