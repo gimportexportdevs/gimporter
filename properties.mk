e8c469f1a86bc974f651664c6413c5699e518b4b
Harald Hoyer <harald@hoyer.xyz>
2019-04-23 16:21:08 +0200

    add MARQ devices

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
-DEVICE ?= fenix5plus
-SDK_HOME = $(HOME)/connect-iq
+DEVICE ?= edge1030
+SDK_HOME = /Users/harald/connectiq/connectiq-sdk-mac-3.0.10-2019-4-9-b0d8876
 DEPLOY = /run/media/$(USER)/GARMIN/GARMIN/APPS
 PRIVATE_KEY = $(HOME)/.id_rsa_garmin.der
