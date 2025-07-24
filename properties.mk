e71f29011fb13c64dcdda5a96eedd3fa376c5c45
Harald Hoyer <harald@hoyer.xyz>
2025-07-24 08:02:01 +0200

    chore: update default DEVICE to marqadventurer

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
-DEVICE ?= fr945
+DEVICE ?= marqadventurer
 SDK_HOME = $(shell cat $(HOME)/Library/Application\ Support/Garmin/ConnectIQ/current-sdk.cfg)
 DEPLOY = $(HOME)
 PRIVATE_KEY = $(HOME)/.id_rsa_garmin.der
